66182bf7ec2bfc1adea5ae13dfc75344d38745ba
Winnard Arthur <jadenxmith@gmail.com>
2023-02-24 17:44:34 +0000

    create google docs clone

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Document = require('./document');
+
+mongoose.connect('mongodb+srv://JadenXmith:Gibbwizze,.7@cluster0.hr3srcd.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('connected'))
+    .catch((err) => console.log(err))
+
+const io = require('socket.io')(5000, {
+    cors: {
+        origin: 'http://localhost:3001',
+        method: ['GET', 'POST']
+    },
+})
+
+io.on('connection', socket => {
+    socket.on('get-document', async documentId => {
+        const document = await findOrCreateDocument(documentId);
+        socket.join(documentId)
+        socket.emit('load-document', document.data) 
+        
+        socket.on('send-changes', delta => {
+            socket.broadcast.to(documentId).emit('receive-changes', delta)
+        })
+
+        socket.on('save-document', async data => {
+            await Document.findByIdAndUpdate(documentId, { data })
+        })
+    })
+
+})
+
+const defaultValue = "";
+
+async function findOrCreateDocument(id) {
+    if (id == null) return;
+
+    const document = await Document.findById(id);
+    if (document) return document;
+
+    return await Document.create({_id: id, data: defaultValue })
+}
