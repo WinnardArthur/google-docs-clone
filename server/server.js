fad00cc4607dd3bed2a57ce668fb7c7578486c9b
Winnard Arthur <jadenxmith@gmail.com>
2023-02-27 17:17:43 +0000

    require and initialize express properly

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+const express = require('express');
 const mongoose = require('mongoose');
 const Document = require('./document');
-const app = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const app = express();
 
 app.use(cors());
 
